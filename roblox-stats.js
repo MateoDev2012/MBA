0e3bbc613cd16c9d68812d5c356aad24bee42e8d
MateoDev2012 <mateodev2012@gmail.com>
2026-09-26 15:06:03 -0500

    Revert deliberate error

--- roblox-stats.js
+++ roblox-stats.js
@@ -122,7 +122,6 @@
   'use strict';
 
   console.error('[roblox-stats] SCRIPT START');
-  throw new Error('[roblox-stats] DELIBERATE ERROR TO TEST CONSOLE');
 
   // ==========================================================================
   // Moonlight Studios — attribution
